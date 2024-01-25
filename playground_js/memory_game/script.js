e858785f7c8bd00fed12a227da532f1c29b450a7
Maria <abc@example.com>
2024-01-25 19:03:51 +0200

    add

--- playground_js/memory_game/script.js
+++ playground_js/memory_game/script.js
@@ -1,5 +1,55 @@
+const cards = document.querySelector("smallCards");
+
+function creatCards() {
+
+    for (i = 0; i < 16; i++) {
+        const bigD = document.getElementById("bigD");
+        const smallCards = document.createElement('div');
+
+        smallCards.innerText = "Small card";
+
+        bigD.append(smallCards);
+
+        smallCards.classList.add('smallCards');
+        bigD.classList.add('bigD');
 
 
+    }
+    cards.addEventListener('click', flipCard);
+}
+
+function flipCard() {
+    cards.forEach(element => {
+        const hiddenDiv = document.createElement('div');
+        innerDiv.classList.add('innerDiv');
+
+        hiddenDiv.querySelectorAll('innerDiv');
+    })
+}
+flipCard();
+
+
+const theCard = document.querySelector(".smallCards");
+
+theCard.addEventListener('click', flipCard);
+
+
+
+//add same picture to 2 divs
+//add a condition that is they fliped togather ==true
+
+
+/*
+
+functions
+
+2.chak right conifgrtion(if two ==right)
+3.leave the right one open
+4.
+
+*/
+
+/*
 function creatCards() {
 
     for (i = 0; i < 9; i++) {
@@ -15,27 +65,4 @@ function creatCards() {
 
     }
 }
-
-// make the divs even(!9)number 16?
-//diplay divs in a table(did grid ) 
-//move to  bigDiv!!!!
-
-
-//make the mainDiv have two inner divs
-//make them trsntion from hiden 180deg on ev=>click
-
-
-//add same picture to 2 divs
-//add a condition that is they fliped togather ==true
-
-
-/*
-
-functions
-
-1.creat divs
-2.chak right conifgrtion(if two ==right)
-3.leave the right one open
-4.
-
 */
