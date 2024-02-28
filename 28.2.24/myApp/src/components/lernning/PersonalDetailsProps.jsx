dd274f593a5f3783ef824a4feed222ef074145bc
Maria <abc@example.com>
2024-02-28 19:07:15 +0200

    add

---
+++ 28.2.24/myApp/src/components/lernning/PersonalDetailsProps.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PersonalDetailsProps extends Component {
+    render() {
+        return (
+            <div>PersonalDetailsProps</div>
+        )
+    }
+}
+
+rcc
