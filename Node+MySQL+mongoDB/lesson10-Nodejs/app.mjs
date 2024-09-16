a545f59c189668fa98c02f33626ec2688e079627
Maria <abc@example.com>
2024-09-16 18:35:54 +0300

    add

---
+++ Node+MySQL+mongoDB/lesson10-Nodejs/app.mjs
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from 'cors';
+
+const app = express();
+
+app.listen(4444, () => {
+    console.log("server is running on port 4444")
+});
+
+app.get("/", (req, res) => {
+    res.send({
+        message: "Hello world!",
+    })
+});
